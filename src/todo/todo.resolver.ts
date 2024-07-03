14dd703f54ca27bed82f72f435044fc72f19177e
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2024-07-03 13:42:00 -0400

    feat(003): entity object type and decoratos

---
+++ src/todo/todo.resolver.ts
@@ -0,0 +1,23 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { Todo } from './entity/todo.entity';
+
+@Resolver()
+export class TodoResolver {
+
+    @Query(()=>[Todo], {name: "todos"})
+    findAll(): Todo[] {
+        return [];
+    }
+
+    findOne(){
+        return [];
+    }
+
+    updateTodo(){
+        return [];
+    }
+
+    removeTodo(){
+        return [];
+    }
+}
