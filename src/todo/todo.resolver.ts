2c83b1964da149eb500163fe0598763d87dc6562
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2024-07-04 01:29:22 -0400

    feat(004): crud update

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,20 +1,34 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
+import { TodoService } from './todo.service';
+import { CreateTodoInput, UpdateTodoInput } from './dto/inputs';
+
 
 @Resolver()
 export class TodoResolver {
 
+    constructor(private readonly todoService: TodoService){
+    }
+
     @Query(()=>[Todo], {name: "todos"})
     findAll(): Todo[] {
-        return [];
+        return this.todoService.findAll();
     }
 
-    findOne(){
-        return [];
+    @Query(()=>Todo, {name: "todo"})
+    findOne(@Args('id', { type: () => Int } ) id: number ): Todo  {
+        return this.todoService.findOne(id);
     }
 
-    updateTodo(){
-        return [];
+    @Mutation( () => Todo, {name: "createTodo"})
+    createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput ){
+        
+        return this.todoService.create(createTodoInput);
+    }
+
+    @Mutation( () => Todo, {name: "updateTodo"})
+    updateTodo(@Args('updateTodoInput') updateTodoInput: UpdateTodoInput){
+        return this.todoService.update( updateTodoInput )
     }
 
     removeTodo(){
